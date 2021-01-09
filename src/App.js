e03e21a4d85fe8c925676b90ffc59036c831b8f5
ignacioalta <altamiranoignaciodso@gmail.com>
2021-01-09 16:53:54 -0300

    created project and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import './App.css';
+import HomePage from './homePageComponent';
+
+function App() {
+  return (
+    <Fragment>
+      <HomePage />
+    </Fragment>
+  );
+}
+
+export default App;

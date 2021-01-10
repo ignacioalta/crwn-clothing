8c9a69d9ac84243262e43f5b7fe42ee7c446f0d7
ignacioalta <altamiranoignaciodso@gmail.com>
2021-01-10 18:52:59 -0300

    finished styling menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import './App.css';
-import HomePage from './homePageComponent';
+import HomePage from './pages/homepage/HomePage';
 
 function App() {
   return (
